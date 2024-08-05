f1ee0edbc8771cd6ac4910f38b159828940bc60a
ibrahim <ibrahim.merkezlol@gmail.com>
2024-08-05 20:47:33 +0200

    first comit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/my-own-crypto",
   plugins: [react()],
-  base: "/my-own-crypto/", // YOUR REPO NAME HERE
-
 })
 
